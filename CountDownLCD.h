8794449399f004e89bf111fb37847907e080f2e3
ndremov <ndremov@sfxdx.ru>
2020-01-10 00:11:17 +0300

    Create countdown lcd

--- CountDownLCD.h
+++ CountDownLCD.h
@@ -1,30 +1,18 @@
-#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include "CountDown.h"
 
 #ifndef CountDownLCD_h
 #define CountDownLCD_h
 
-class CountDownLCD : CountDown
+class CountDownLCD
 {
 private:
     LiquidCrystal_I2C *_lcd;
     CountDown *_countDown;
 
 public:
-    CountDownLCD(LiquidCrystal_I2C *lcd, CountDown *countDown)
-    {
-        _lcd = lcd;
-        _countDown = countDown;
-    }
-
-    printCountDown()
-    {
-        _lcd->setCursor(0, 0);
-        double progress = _countDown->getProgress();
-        _lcd->print((int)progress);
-        _lcd->print("%");
-    }
+    CountDownLCD(LiquidCrystal_I2C *lcd, CountDown *countDown);
+    printCountDown();
 };
 
 #endif //CountDownLCD.h
