6a21d643dec3d35df6fe6484f1fc59fec8ebe9e9
ndremov <ndremov@sfxdx.ru>
2020-01-18 19:00:19 +0300

    Exclude gun chronograph

--- GunChronograph.h
+++ GunChronograph.h
@@ -24,83 +24,16 @@ private:
     unsigned long _endTime = 0;
 
 public:
-    initLCD(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
-    {
-        _lcd = new LiquidCrystal(rs, enable, d4, d5, d6, d7);
-        _lcd->begin(16, 2);
-        _partialLCD = new PartialLCD(_lcd);
-        _speedLCD = new SpeedLCD(_lcd);
-    }
-
-    initPhotoresistors(uint8_t firstPort, uint8_t secondPort, unsigned int maximalSetupIteration)
-    {
-        _firstPhotoresistor = new Photoresistor(firstPort, maximalSetupIteration);
-        _secondPhotoresistor = new Photoresistor(secondPort, maximalSetupIteration);
-    }
-
-    initPotentiometer(uint8_t port)
-    {
-        _potentiometer = new Potentiometer(port);
-    }
-
-    startSetupPhotoresistor()
-    {
-        _partialLCD->printName("setup");
-    }
-
-    bool setupPhotoresistor()
-    {
-        _firstPhotoresistor->nextSetup();
-        _secondPhotoresistor->nextSetup();
-
-        _partialLCD->printPartial((Partial *)this);
-
-        return this->getCompleteSetupStatus();
-    }
-
-    bool setupPotentiometer()
-    {
-        _partialLCD->printName("sensitivity");
-    }
-
-    void checkPotentiometer()
-    {
-        _partialLCD->printPartial((Partial *)this);
-    }
-
-    bool checkSpeed()
-    {
-        if (_startTime == 0 && _firstPhotoresistor->checkTurnOff(_potentiometer->getPartValue()))
-        {
-            _startTime = micros();
-            return false;
-        }
-        if (_startTime != 0 && _secondPhotoresistor->checkTurnOff(_potentiometer->getPartValue()))
-        {
-            _endTime = micros();
-            double speed = tubeLength / (_endTime - _startTime);
-            _speedLCD->printSpeed(speed);
-            _startTime = 0;
-            _endTime = 0;
-            return true;
-        }
-        return false;
-    }
-
-    double getPartValue()
-    {
-        if (this->getCompleteSetupStatus())
-        {
-            return _potentiometer->getPartValue();
-        }
-
-        return (_firstPhotoresistor->getPartValue() + _secondPhotoresistor->getPartValue()) / 2;
-    }
-
-    bool getCompleteSetupStatus()
-    {
-        return _firstPhotoresistor->getCompleteSetupStatus() && _secondPhotoresistor->getCompleteSetupStatus();
-    }
+    initLCD(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+    initPhotoresistors(uint8_t firstPort, uint8_t secondPort, unsigned int maximalSetupIteration);
+    initPotentiometer(uint8_t port);
+    startSetupPhotoresistor();
+    bool setupPhotoresistor();
+    bool setupPotentiometer();
+    void checkPotentiometer();
+    bool checkSpeed();
+    double getPartValue();
+    bool getCompleteSetupStatus();
 };
 
 #endif //GunChronograph.h
